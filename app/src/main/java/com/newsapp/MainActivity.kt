e622608b6cf58a7ad7792cde98471da2312ecc4e
emmyalexjnr <emmyalexjnr@gmail.com>
2024-07-04 20:44:40 +0100

    another change

--- app/src/main/java/com/newsapp/MainActivity.kt
+++ app/src/main/java/com/newsapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             NewsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android news",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
